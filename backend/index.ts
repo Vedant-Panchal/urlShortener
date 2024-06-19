d77fe7faed9634a738b70d228103d548e35210c9
NotTheRightGuy <janmejaychatterjee@gmail.com>
2024-06-19 17:34:55 +0530

    chore: Update .gitignore and remove unused .env.local file

--- backend/index.ts
+++ backend/index.ts
@@ -1,22 +1,22 @@
-import express, { Express } from 'express';
-import dotenv from 'dotenv';
-import router from './routes/url';
-import connectMongo from './connect';
-import urlModel from './models/url';
-
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import router from "./routes/url";
+import connectMongo from "./connect";
+import urlModel from "./models/url";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 const URL = process.env.URL;
+console.log(URL);
 if (!URL) {
-  throw new Error('URL is not defined in the environment variables');
+    throw new Error("URL is not defined in the environment variables");
 }
 connectMongo(URL).then(() => console.log(`Mongo connected !!`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/v1/url', router);
+app.use("/api/v1/url", router);
 
 // app.get('/:shortId', async (req, res) => {
 //   const shortId = req.params.shortId;
@@ -31,8 +31,10 @@ app.use('/api/v1/url', router);
 //         }
 //       }
 //     })
-    
+
 //     res.redirect(data.originalUrl);
 // })
 
-app.listen(port, () => console.log(`Server is running on http://localhost:${port}/api/v1/url`));
+app.listen(port, () =>
+    console.log(`Server is running on http://localhost:${port}/api/v1/url`)
+);
--- backend/routes/url.ts
+++ backend/routes/url.ts
@@ -1,9 +1,7 @@
 import express, { Request, Response } from "express";
-import { handleGenerateNewShortUrl, running} from "../controllers/url";
+import { handleGenerateNewShortUrl, running } from "../controllers/url";
 const router = express.Router();
 
-router
-.get('/',running)
-.post("/new",handleGenerateNewShortUrl);
+router.get("/", running).post("/new", handleGenerateNewShortUrl);
 
 export default router;
